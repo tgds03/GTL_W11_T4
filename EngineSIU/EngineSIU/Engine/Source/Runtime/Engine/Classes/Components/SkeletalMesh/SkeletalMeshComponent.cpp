50c9e5d99a2327998a9d8ba671c79b848620ca54
chanhuiHan <hch3245@naver.com>
2025-05-03 16:54:04 +0900

    feat: SkinnedMeshComponent를 상속 받는 SkeletalMeshComponent 내용 채움,

--- EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Components/SkeletalMesh/SkeletalMeshComponent.cpp
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Components/SkeletalMesh/SkeletalMeshComponent.cpp
@@ -1,2 +1,7 @@
 #include "SkeletalMeshComponent.h"
 
+USkeletalMeshComponent::USkeletalMeshComponent()
+    :USkinnedMeshComponent()
+{
+
+}
--- EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Components/SkeletalMesh/SkeletalMeshComponent.h
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Components/SkeletalMesh/SkeletalMeshComponent.h
@@ -1,2 +1,20 @@
 #pragma once
 
+#include "UObject/Object.h"
+#include "UObject/ObjectMacros.h"
+#include "Engine/Source/Runtime/Launch/SkeletalDefine.h"
+#include "Engine/Source/Runtime/Core/Container/Array.h"
+#include "Engine/Source/Runtime/Engine/Classes/Components/SkeletalMesh/SkinnedMeshComponent.h"
+
+
+class USkeletalMeshComponent : public USkinnedMeshComponent
+{
+    DECLARE_CLASS(USkeletalMeshComponent, USkinnedMeshComponent)
+
+public:
+    USkeletalMeshComponent();
+    ~USkeletalMeshComponent() = default;
+
+    // 현재는 USkeletalMeshComponent에서 SkinnedMeshComponent에 비해 더 해주는 것이 없음
+    // 이후에 애니메이션 쪽 구현이 들어갈 때 추가된다고 GPT 가 말하긴 함
+};
