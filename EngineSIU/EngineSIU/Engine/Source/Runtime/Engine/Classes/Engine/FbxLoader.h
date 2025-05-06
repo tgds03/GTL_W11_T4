1257ec6c1434267118c7cb4d90a39ec6953ebd96
chanhuiHan <hch3245@naver.com>
2025-05-06 23:29:51 +0900

    fix: Tangent Vector 문제 해결, Thanks to Namhyun

--- EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Engine/FbxLoader.h
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Engine/FbxLoader.h
@@ -5,6 +5,9 @@
 #include "UObject/Object.h"
 #include "Components/SkeletalMesh/SkeletalMesh.h"
 
+
+struct FSkeletalMeshVertex;
+
 struct FFbxLoader 
 {
 public:
@@ -13,6 +16,10 @@ public:
     static USkeletalMesh* GetSkeletalMesh(const FWString& name);
 
     static bool CreateTextureFromFile(const FWString& Filename, bool bIsSRGB = true);
+
+    static void CalculateTangent(FSkeletalMeshVertex& PivotVertex, const FSkeletalMeshVertex& Vertex1, const FSkeletalMeshVertex& Vertex2);
+
+    static FbxVector4 GetNormalMappingVector(FbxGeometryElementNormal* normalElem, FbxMesh* mesh, int polygonIndex, int vertIndex);
 
 private:
     inline static TMap<FWString, USkeletalMesh*> SkeletalMeshMap;
